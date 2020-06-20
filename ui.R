c63e3dfae9bbeca737fce3de24966ca68ebf5ac1
Armin Najarpour Foroushani <armin.najarpour@gmail.com>
2020-06-20 13:09:56 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,20 +1,3 @@
-# This application predicts the Species label of a flower given
-#the petal and sepal length and width for the flower.
-
-# The model is trained with Random Forest and all four features
-# in the Iris dataset are being used. 150 data samples in this 
-# dataset are divided into 70% for training and 30% for testing.
-
-# You can choose any number of test data using the sidebar
-# which is between 1 to 45 which is included in the test set
-# The output plot shows which samples are predicted correctly
-# and which are not.
-# Using radio buttons, you can select to show different
-# information on Y and X axes (features).
-# The App also presents predicted values at the bottom.
-
-# when you run the App, it takes few seconds for the plot to
-# appear.
 
 library(shiny)
 shinyUI(fluidPage(
